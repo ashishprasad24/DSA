b52a660e5931e1ea61cebbaeaf3b71a3e26fd902
ashishprasad24 <86977006+ashishprasad24@users.noreply.github.com>
2021-08-06 21:54:13 +0530

    Create smallerthancurrent.py

---
+++ leeetcode/smallerthancurrent.py
@@ -0,0 +1,13 @@
+#How Many Numbers Are Smaller Than the Current Numbe
+class Solution:
+    def smallerNumbersThanCurrent(self, nums):
+        l=len(nums)
+        ans=[]
+        count=0
+        for i in nums:
+            for j in range(l):
+               if (nums[j]-i)<0:
+                   count+=1
+            ans.append(count)
+            count=0
+        return ans
