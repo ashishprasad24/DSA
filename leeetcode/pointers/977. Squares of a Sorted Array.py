06f355e45f266c589e112733d17c387833d437c5
ashishprasad24 <86977006+ashishprasad24@users.noreply.github.com>
2021-09-23 21:53:11 +0530

    Create 977. Squares of a Sorted Array.py

---
+++ leeetcode/pointers/977. Squares of a Sorted Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        result = [None for _ in A]
+        left, right = 0, len(A) - 1
+        for index in range(len(A)-1, -1, -1):
+            if abs(A[left]) > abs(A[right]):
+                result[index] = A[left] ** 2
+                left += 1
+            else:
+                result[index] = A[right] ** 2
+                right -= 1
+        return result
+        
