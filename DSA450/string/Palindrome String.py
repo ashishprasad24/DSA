f0186ef3e241e17088095cf408192fe28444fb53
ashishprasad24 <86977006+ashishprasad24@users.noreply.github.com>
2021-11-22 22:50:52 +0530

    Create Palindrome String.py

---
+++ DSA450/string/Palindrome String.py
@@ -0,0 +1,14 @@
+#User function Template for python3
+class Solution:
+	def isPalindrome(self, S):
+	    
+	    l=0
+	    r=len(S)-1
+	    
+	    while l<r:
+	        if S[l]!=S[r]:
+	            return 0
+	            
+	        l+=1
+	        r-=1
+	    return 1
