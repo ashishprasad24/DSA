f478f04d762a956bf672d35c6a16df626c12628a
ashishprasad24 <86977006+ashishprasad24@users.noreply.github.com>
2021-12-03 12:17:51 +0530

    Create 2057. Smallest Index With Equal Value.py

---
+++ leeetcode/ARRAY/2057. Smallest Index With Equal Value.py
@@ -0,0 +1,7 @@
+lass Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if i%10==nums[i]:
+                return i
+        return -1
+        
