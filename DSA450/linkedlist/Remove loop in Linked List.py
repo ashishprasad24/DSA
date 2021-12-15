48add68c9f16dac9f3d39b578f80c7ba71451039
ashishprasad24 <86977006+ashishprasad24@users.noreply.github.com>
2021-12-15 23:30:15 +0530

    Create Remove loop in Linked List.py

---
+++ DSA450/linkedlist/Remove loop in Linked List.py
@@ -0,0 +1,50 @@
+
+class Solution:
+    #Function to remove a loop in the linked list.
+    def removeLoop(self, head):
+        
+        #using two pointers and moving one pointer(slow) by one node and 
+        #another pointer(fast) by two nodes in each iteration.
+        fast = head.next
+        slow = head
+        
+        while fast!=slow:
+            
+            #if the node pointed by first pointer(fast) or the node 
+            #next to it is null, then loop is not present so we return 0.
+            if fast is None or fast.next is None:
+                return
+            
+            fast = fast.next.next
+            slow = slow.next
+            
+        #both pointers now point to the same node in the loop.
+        
+        size = 1
+        fast = fast.next
+    
+        #we start iterating the loop with first pointer and continue till 
+        #both pointers point to same node again.
+        while fast!=slow:
+            fast = fast.next
+            #incrementing the counter which stores length of loop.
+            size+=1
+        
+        #updating the pointers again to starting node.
+        slow=head
+        fast=head
+        
+        #moving pointer (fast) by (size-1) nodes.
+        for _ in range(size-1):
+            fast=fast.next
+        
+        #now we keep iterating with both pointers till fast reaches a node such
+        #that the next node is pointed by slow. At this situation slow is at
+        #the node where loop starts and first at last node so we simply 
+        #update the link part of first.
+        while fast.next != slow:
+            fast=fast.next
+            slow=slow.next
+        
+        #storing null in link part of the last node.
+        fast.next=None
